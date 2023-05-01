da35286be2f0cefed16303b4ec01b2d71984f55d
marcelochincha <103434714+marcelochincha@users.noreply.github.com>
2023-05-01 11:14:00 -0500

    Query update!

--- deltr0n_system/urls.py
+++ deltr0n_system/urls.py
@@ -8,7 +8,12 @@ router = routers.DefaultRouter()
 router.register(r'productos', views.ProductoView, 'Producto')
 router.register(r'clientes', views.ClienteView, 'Cliente')
 
+
+queryRouter = routers.DefaultRouter()
+queryRouter.register(r'productos', views.ProductoQueryView, 'ProductoQuery')
+
 urlpatterns = [
+    path('api/search/', include(queryRouter.urls)),
     path("api/v1/", include(router.urls)),
     path('docs/', include_docs_urls(title="Deltr0n API"))
 ]
