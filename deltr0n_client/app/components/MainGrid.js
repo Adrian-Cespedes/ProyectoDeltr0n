e881889b65e2a76b63c65f9ea95d0885664b50c8
Lenin Gerardo Chavez Zapata <lenin.chavez@utec.edu.pe>
2023-06-11 21:17:31 -0500

    Update HomePage
    Links to Escribenos, carrito and the account page are in process

--- deltr0n_client/app/components/MainGrid.js
+++ deltr0n_client/app/components/MainGrid.js
@@ -3,40 +3,26 @@ import Grid from '@mui/material/Grid'; // Grid version 1
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import ProductCard from './ProductCard';
+import Container from "@mui/material/Container";
+import Image from "next/image";
+import Centerimage from "../../public/CENTERIMAGE.jpg"
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(1), 
   textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
 function MainGrid() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={4}>
-        <Item>xs=8</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={12}>
-        <Item>xs=8</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=8</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-      <Grid item xs={4}>
-        <Item>xs=4</Item>
-      </Grid>
-    </Grid>
+   <Container sx={{display: "flex", alignItems:"center", justifyItems:"center", justifyContent:"center"}}>
+      <Container sx = {{display: "flex"}}>
+        <Image src= {Centerimage} alt='centerimage' width={1000} height={500}/>
+      </Container>
+   </Container>
+
   )
 }
 
--- deltr0n_client/app/components/ResponsiveAppBar.js
+++ deltr0n_client/app/components/ResponsiveAppBar.js
@@ -1,22 +1,55 @@
-'use client'
+"use client";
 
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import Image from "next/image";
+import logo from "../../public/logo.png";
+import Input from "@mui/material/Input";
+import { style } from "@mui/system";
 
-const pages = ['Productos', 'Pricing', 'Blog'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const Logito = (props) => (
+  <Image
+    priority={true}
+    src={logo}
+    alt="Picture of the author"
+    width={100}
+    height={100}
+  />
+);
+const links = [
+  {
+    label: "Escribenos",
+    url: "/contacto",
+    img: "https://static.vecteezy.com/system/resources/previews/018/930/486/original/telegram-logo-telegram-icon-transparent-free-png.png",
+    styleconfig: {
+      width: "100px",
+      height: "100px",
+      padding: "0px",
+    },
+  },
+  {
+    label: "Carrito",
+    url: "/carrito",
+    img: "https://icon-library.com/images/shopping-cart-icon-white/shopping-cart-icon-white-12.jpg",
+    styleconfig: {
+      width: "40px",
+      height: "40px",
+    },
+  },
+];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -38,10 +71,10 @@ function ResponsiveAppBar() {
   };
 
   return (
-    <AppBar position="static">
+    <AppBar position="static" sx={{ backgroundColor: "black" }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Logito />
           <Typography
             variant="h6"
             noWrap
@@ -49,18 +82,18 @@ function ResponsiveAppBar() {
             href="/"
             sx={{
               mr: 2,
-              display: { xs: 'none', md: 'flex' },
-              fontFamily: 'monospace',
+              display: { xs: "none", md: "flex", padding: "20px" },
+              fontFamily: "monospace",
               fontWeight: 700,
               // letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              color: "inherit",
+              textDecoration: "none",
             }}
           >
-            PC Hermanos
+            PCUnion
           </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
@@ -75,28 +108,28 @@ function ResponsiveAppBar() {
               id="menu-appbar"
               anchorEl={anchorElNav}
               anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
+                vertical: "bottom",
+                horizontal: "left",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
+                vertical: "top",
+                horizontal: "left",
               }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                display: { xs: 'block', md: 'none' },
+                display: { xs: "block", md: "none" },
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+              {links.map(({ label, url }) => (
+                <MenuItem key={url} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="end">{label}</Typography>
                 </MenuItem>
               ))}
             </Menu>
           </Box>
-          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
           <Typography
             variant="h5"
             noWrap
@@ -104,47 +137,62 @@ function ResponsiveAppBar() {
             href=""
             sx={{
               mr: 2,
-              display: { xs: 'flex', md: 'none' },
+              display: { xs: "flex", md: "none" },
               flexGrow: 1,
-              fontFamily: 'monospace',
+              fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          ></Typography>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              gap: 5,
+              justifyContent: "end",
             }}
           >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
+            {links.map(({ label, url, img, styleconfig }) => (
               <Button
-                key={page}
+                key={url}
                 onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
+                sx={{ my: 2, color: "white", display: "flex" }}
               >
-                {page}
+                <img src={img} alt="" style={styleconfig} />
+                {label}
               </Button>
             ))}
           </Box>
 
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Configuración">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
+              <Button
+                onClick={handleOpenUserMenu}
+                sx={{ p: 5, color: "white", display: "flex" }}
+              >
+                <img
+                  src="https://giftcards.woolworths.com.au/_ui/responsive/common/images/User_icon_white.png"
+                  alt=""
+                  style={{ width: "30px", height: "30px", padding: "0 5" }}
+                />
+                Mi cuenta
+              </Button>
             </Tooltip>
             <Menu
-              sx={{ mt: '45px' }}
+              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               keepMounted
               transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
+                vertical: "top",
+                horizontal: "right",
               }}
               open={Boolean(anchorElUser)}
               onClose={handleCloseUserMenu}
@@ -157,6 +205,19 @@ function ResponsiveAppBar() {
             </Menu>
           </Box>
         </Toolbar>
+      </Container>
+      <Container maxWidth="xl" sx={{ display: "flex" }}>
+        <Input
+          placeholder="🔎 Buscar artículo"
+          sx={{
+            display: { backgroundColor: "#ffffff" },
+            padding: "10px",
+            borderRadius: "7px",
+            flexGrow: 1,
+            left: "35%",
+            maxWidth: "62%",
+          }}
+        />
       </Container>
     </AppBar>
   );
