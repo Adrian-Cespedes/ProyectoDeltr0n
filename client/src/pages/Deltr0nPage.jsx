8127f01ea0138da79bf3401d53eb63eb805a2199
Lenin Chavez <lenin.chavez@utec.edu.pe>
2023-06-15 14:29:54 -0500

    CLEAN DEV BRANCH

--- client/src/pages/Deltr0nPage.jsx
+++
@@ -1,5 +0,0 @@
-import {Deltr0nList} from '../components/Deltr0nList'
-
-export function Deltr0nPage() {
-    return <Deltr0nList/>;
-}
--- deltr0n_system/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class Deltr0NSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'deltr0n_system'
