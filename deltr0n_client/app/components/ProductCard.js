21f5412d20bc9f1b653f956ae9d50b6ddc836afd
Lenin Chavez <lenin.chavez@utec.edu.pe>
2023-07-12 13:50:50 -0500

    Finish signup page

--- deltr0n_client/app/components/ProductCard.js
+++ deltr0n_client/app/components/ProductCard.js
@@ -1,8 +1,8 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 export default function ProductCard({ product }) {
   return (
@@ -16,15 +16,17 @@ export default function ProductCard({ product }) {
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {product.nombre ? product.nombre : 'Nombre faltante...'}
+            {product.nombre ? product.nombre : "Nombre faltante..."}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {product.descripcion ? product.descripcion : 'Descripcion faltante...'}
+            {product.descripcion
+              ? product.descripcion
+              : "Descripcion faltante..."}
           </Typography>
-          <Typography variant="body2" className='text-red-500 font-bold'>
+          <Typography variant="body2" className="text-red-500 font-bold">
             Stock: {product.stock ? product.stock : 0}
           </Typography>
-          <Typography variant="body2" className='text-gray-800 font-bold'>
+          <Typography variant="body2" className="text-gray-800 font-bold">
             S/. {product.precio ? product.precio : 0}
           </Typography>
         </CardContent>
